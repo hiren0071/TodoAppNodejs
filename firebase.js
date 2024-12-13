5e18a55d965ec1e8cd050af7e0d8075e82142852
hiren0071 <41529145+hiren0071@users.noreply.github.com>
2024-12-13 02:20:46 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,16 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./firebasekey/todobackend-f3a80-firebase-adminsdk-ok5it-27445c466c.json');
+const serviceAccount = require('./firebasekey/todobackend-f3a80-firebase-adminsdk-ok5it-980cc129dd.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://todobackend-f3a80.firebaseio.com',
+});
+
+
+
+module.exports = admin; 
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
